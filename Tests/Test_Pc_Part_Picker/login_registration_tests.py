f80d7a396f80b73c47d5aace68043f6fe14a2157
jameelnassar <jameelna978@gmail.com>
2024-03-09 21:07:55 +0200

    remove useless code

--- Tests/Test_Pc_Part_Picker/login_registration_tests.py
+++ Tests/Test_Pc_Part_Picker/login_registration_tests.py
@@ -38,116 +38,87 @@ class login_registration_tests(unittest.TestCase):
         current_page = Login_Registration_Page(CAP)
         current_page.account_login_flow(self.variables['correct_username'], self.variables['correct_password'])
         result = current_page.check_if_user_is_logged_in()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
-
+        self.assertTrue(result)
+        current_page.close_page()
+        
     def test_login_with_wrong_password(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.account_login_flow(self.variables['correct_username'], self.variables['incorrect_username'])
         result = current_page.check_if_user_entred_wrong_credentials()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_login_with_wrong_Username(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.account_login_flow(self.variables['incorrect_username'], self.variables['correct_password'])
         result = current_page.check_if_user_entred_wrong_credentials()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
 
     def test_successful_registration(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(),self.variables['correct_password'],self.rand_email)
         result = current_page.check_if_registration_is_complete()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_already_registered_username(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(self.variables['correct_username'],self.variables['correct_password'],self.rand_email)
         result = current_page.check_for_already_registered_username_error()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_invalid_username(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(self.variables['invalid_username'],self.variables['correct_password'],self.rand_email)
         result = current_page.check_for_invalid_username()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
-
+        self.assertTrue(result)
+  
     def test_registration_with_already_registered_email(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(),generate_random_password(),self.variables['correct_email'])
         result = current_page.check_for_already_registered_email()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_invalid_email(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(), self.variables['correct_password'], self.rand_invalid_email)
         result = current_page.check_for_invalid_email()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_not_matching_email(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(), self.variables['correct_password'], self.rand_email, self.wrong_email )
         result = current_page.check_for_not_matching_email()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_not_matching_password(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(), self.variables['correct_password'],password_repeat=self.variables['correct_password'] + 'a' , email = self.rand_email)
         result = current_page.check_for_not_matching_password()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
 
     def test_registration_with_invalid_password(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(), self.variables["invalid_password"], email = self.rand_email)
         result = current_page.check_for_registration_error()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_not_checking_tos(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(),self.variables['correct_password'],self.rand_email,TOS=False)
         result = current_page.check_for_registration_error()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
+
 
     def test_registration_with_not_checking_ucoc(self,CAP = None):
         current_page = Login_Registration_Page(CAP)
         current_page.register_account_flow(generate_random_username(),self.variables['correct_password'],self.rand_email,UCOC=False)
         result = current_page.check_for_registration_error()
-        try:
-            self.assertTrue(result)
-        except:
-            current_page.close_page()
+        self.assertTrue(result)
