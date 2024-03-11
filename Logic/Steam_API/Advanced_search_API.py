16a384d5bad9f1bde271758ccaac37c40d3c00fe
Jameel Nassar <jameelna978@gmail.com>
2024-03-11 10:44:17 +0200

    Added .yml file and some code changes

--- Logic/Steam_API/Advanced_search_API.py
+++ Logic/Steam_API/Advanced_search_API.py
@@ -5,8 +5,8 @@ import os
 
 class Advanced_search_API:
 
-    def __init__(self):
-        self.my_api = APIWrapper()
+    def __init__(self,my_api):
+        self.my_api = my_api
         cur_dir = os.path.dirname(os.path.abspath(__file__))
         self._config = read_json(os.path.join(cur_dir, "API_Configs", "Advanced_search_API.json"))
         self.URL = self._config['url']
@@ -15,25 +15,15 @@ class Advanced_search_API:
         search_term = search_term.replace(" ","+")
         return f'{self.URL}?term={search_term}&json=1'
 
-    def prepair_url_params(self, input_):
-        params_dict = {}
-        for key in input_.keys():
-            params = self._config[key]
-            if input_[key] == True:
-                key = params.split("=")[0]
-                val = params.split("=")[1]
-                params_dict[key] = val
-            else:
-                params_dict[params] = input_[key]
-        return params_dict
-
     def get_search_results(self, term, **extra):
         url = self.prepair_api_url(term)
         if extra:
-            params_dict = self.prepair_url_params(extra)
-            url = add_query_Parameters_to_api_request(url, params_dict)
-        result = self.my_api.api_get_request(url)
-        return result.json()
+            result = self.my_api.api_get_request_with_param(url, extra,self._config)
+            return result.json()
+        else:
+            result = self.my_api.api_get_request(url)
+            return result.json()
+
 
     def get_app_ids_from_search_results(self,result):
         items = result["items"]
