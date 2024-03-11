52e2660918a47684a28092b3ee8dbb53fb0c7908
Jameel Nassar <jameelna978@gmail.com>
2024-03-11 12:40:36 +0200

    Modified Test Runner

--- Test_runner.py
+++ Test_runner.py
@@ -9,38 +9,41 @@ import unittest
 # Import your test modules
 from Tests.Test_Steam_API.Test_app_review_api import *
 
+
 def run_test(current_test):
     suite = unittest.TestSuite()
     suite.addTest(init_test(current_test))
     # Run the test suite
-    current_test_start_time = time.time()
-    result = unittest.TextTestRunner(stream=StringIO(), verbosity=2).run(suite)
-    time_spent = time.time() - current_test_start_time
-    if result.wasSuccessful():
-        print(f'{current_test[1]} Passed!, run time {time_spent:.2f} sec')
-    else:
-        print(f'{current_test[1]} Failed!, run time {time_spent:.2f} sec',end="")
-        print(result.errors)
+    _result = unittest.TextTestRunner(stream=StringIO(), verbosity=2).run(suite)
+    return _result
+
 
 def init_test(input_):
     return input_[0](input_[1])
 
+
 def run_tests_in_parallel(test_cases):
     with ThreadPoolExecutor(max_workers=len(test_cases)) as executor:
-        executor.map(run_test, test_cases)
+        _results = list(executor.map(run_test, test_cases))
+    return _results
 
-def run_tests_in_serrial(test_classes):
-    for test  in test_classes:
-        run_test(test)
 
-def get_unittest_classes(folder_path):
+def run_tests_in_serrial(_test_classes):
+    results_ = []
+    for test in _test_classes:
+        _result = run_test(test)
+        results.append(_result)
+    return results
+
+
+def get_unittest_classes(_folder_path):
     unittest_classes = []
 
     # Iterate through files in the folder
-    for file_name in os.listdir(folder_path):
+    for file_name in os.listdir(_folder_path):
         if file_name.endswith('.py'):
             module_name = os.path.splitext(file_name)[0]
-            module_path = os.path.join(folder_path, file_name)
+            module_path = os.path.join(_folder_path, file_name)
 
             # Load the module
             spec = importlib.util.spec_from_file_location(module_name, module_path)
@@ -60,20 +63,36 @@ if __name__ == "__main__":
     test_classes = get_unittest_classes(folder_path)
     all_test_cases = prepair_all_tests(test_classes)
 
-    #read from config
+    # read from config
     test_config = read_json("Configs/Test_runner.json")
     serial_run = test_config["run_serial"]
     start_time = time.time()
+    results = None
     if serial_run:
         print("running test in serial")
         print("")
-        run_tests_in_serrial(all_test_cases)
+        results = run_tests_in_serrial(all_test_cases)
     else:
         print("running test in parallel")
         print("")
-        run_tests_in_parallel(all_test_cases)
+        results = run_tests_in_parallel(all_test_cases)
     elapsed_time = time.time() - start_time
+    if results == None:
+        raise Exception("Tests Failer to Run")
+    else:
+        test_pass = 0
+        test_fail = 0
+        for result in results:
+            if result.wasSuccessful():
+                test_pass += 1
+            else:
+                print("Test Failed")
+                result.printErrors()
+                test_fail += 1
+        if test_fail == 0:
+            print(f"All tests Passed,number of tests {test_pass}")
+        else:
+            print(f"Some tests Failed,number of failed tests {test_fail}, number of passed tests {test_pass}")
+
     print("")
     print(f"Total Run time: {elapsed_time:.2f} seconds")
-
-
