5daad8b820c5db8d8b8f52541b72ea2578f94425
jameelnassar <jameelna978@gmail.com>
2024-03-18 20:03:51 +0200

    some bug fixes

--- Infra/Env_Variable_wrapper.py
+++ Infra/Env_Variable_wrapper.py
@@ -3,13 +3,13 @@ import sys
 import argparse
 import os.path
 
+
 def create_tokens_file(dictionary, filename='tokens.json'):
     with open(filename, 'w') as file:
         json.dump(dictionary, file)
 
 
 def main():
-
     my_tokens = {
         "JIRA_EMAIL": os.environ['JIRA_EMAIL'],
         "JIRA_TOKEN": os.environ['JIRA_TOKEN'],
@@ -19,7 +19,9 @@ def main():
 
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     config = os.path.join(cur_dir, "tokens.json")
-    create_tokens_file(my_tokens,config)
+    create_tokens_file(my_tokens, config)
+
 
 if __name__ == "__main__":
     main()
+    print(os.environ['JIRA_URL'])
